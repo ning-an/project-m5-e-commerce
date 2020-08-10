f09849f0b92574b53ef5e38ffeb94d9beb0fb6b7
Antonio Placido <aplacido1992@gmail.com>
2020-08-10 14:18:53 -0400

    Added ning's flag / css rendering

--- client/src/components/Items/LargeCompany.js
+++ client/src/components/Items/LargeCompany.js
@@ -1,27 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+import { BuyBtn } from "../BuyBtn";
+import { COLORS } from "../constants";
+
 export default function LargeCompany({ company }) {
+  const [hover, setHover] = useState(false);
   return (
-    <Wrapper>
-      <img
-        src={company.imageSrc}
-        alt="A random product from our inventory to buy"
-      />
-      <p>{company.price}</p>
-      <p>Stock: {company.numInStock}</p>
+    <Wrapper
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <img src={company.imageSrc} alt={company.name} />
       <p>{company.name}</p>
-      <p>{company.category}</p>
+      <p>{company.price}</p>
+      <Flag>{company.category}</Flag>
+      {hover && (
+        <BuyBtn>{company.numInStock > 0 ? "BUY NOW" : "OUT OF STOCK"}</BuyBtn>
+      )}
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   box-shadow: 0px 0px 16px -11px rgba(0, 0, 0, 0.75);
+  padding: 20px 10px 5px;
+  text-align: center;
+  height: 350px;
   img {
     height: 20vh;
     width: 15vw;
@@ -32,4 +42,12 @@ const Wrapper = styled.div`
     margin-right: 10px;
     text-align: center;
   }
+`;
+
+const Flag = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: ${COLORS.bg};
+  color: ${COLORS.font};
 `;
