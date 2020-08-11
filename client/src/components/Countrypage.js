28f3aafa26fe06d32ffdd3268f235ddb652750db
Rupinder Bagri <RBagri94@hotmail.com>
2020-08-11 14:18:50 -0400

    updated

--- client/src/components/Countrypage.js
+++ client/src/components/Countrypage.js
@@ -1,17 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-<<<<<<< Updated upstream
-=======
-import { Grid } from "@material-ui/core";
->>>>>>> Stashed changes
 
 export default function Countrypage() {
   return (
     <>
       <Wrapper>
         <Grid style={{ margin: "50px 100px" }}>
-<<<<<<< Updated upstream
           <Body to="/countries/United States">United States</Body>
           <Body to="/countries/Japan">Japan</Body>
           <Body to="/countries/Taiwan">Taiwan</Body>
@@ -23,19 +18,6 @@ export default function Countrypage() {
           <Body to="/countries/China">China</Body>
           <Body to="/countries/Italy">Italy</Body>
           <Body to="/countries/Austria">Austria</Body>
-=======
-          <Body to="/country/United States">United States</Body>
-          <Body to="/country/Japan">Japan</Body>
-          <Body to="/country/Taiwan">Taiwan</Body>
-          <Body to="/country/Canada">Canada</Body>
-          <Body to="/country/Finland">Finland</Body>
-          <Body to="/country/South Korea">South Korea</Body>
-          <Body to="/country/Netherlands">Netherlands</Body>
-          <Body to="/country/France">France</Body>
-          <Body to="/country/China">China</Body>
-          <Body to="/country/Italy">Italy</Body>
-          <Body to="/country/Austria">Austria</Body>
->>>>>>> Stashed changes
         </Grid>
       </Wrapper>
     </>
