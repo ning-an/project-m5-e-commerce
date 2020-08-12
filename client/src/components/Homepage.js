b958490d5ff7a05adba9e9e66b0af27c6179484c
Ning An <ning.an@outlook.com>
2020-08-12 19:07:43 -0400

    Create homepage slideshow & change web title

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,9 +2,15 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import Pagination from "./Pagination";
+import Slideshow from "./Slides";
 
 export default function Homepage() {
   const { items } = useSelector((state) => state.auth);
 
-  return <Pagination items={items} />;
+  return (
+    <>
+      <Slideshow />
+      <Pagination items={items} />;
+    </>
+  );
 }
